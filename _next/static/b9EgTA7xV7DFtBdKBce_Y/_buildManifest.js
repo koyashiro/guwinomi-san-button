d836872b70d4bafc20ab6d49e8c86c9c879d110a
koyashiro <koyashiro@users.noreply.github.com>
2021-10-24 07:37:34 +0000

    deploy: 7a5dac7857a738865121881f4346af432fe14637

---
+++ _next/static/b9EgTA7xV7DFtBdKBce_Y/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/1bfc9850-14899d3e3d8ca67530fa.js","static/chunks/pages/index-02bec2272fbf0c97dc80.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
